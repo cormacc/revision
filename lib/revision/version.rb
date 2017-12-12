b954e5a5970d4953aa1adb719d63eba178e9537c
Cormac Cannon <cormac.cannon@neuromoddevices.com>
2017-12-12 16:43:40 +0000

    Version 0.1.4 (12 Dec 2017)
    - boo
    - hoo
    - hoo

--- lib/revision/version.rb
+++ lib/revision/version.rb
@@ -1,9 +1,14 @@
 # Defines the revision ID for the revision gem
 module Revision
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
 
 # <BEGIN CHANGELOG>
+# 
+# Version 0.1.4 (12 Dec 2017)
+# - boo
+# - hoo
+# - hoo
 # 
 # Version 0.1.3 (12 Dec 2017)
 # - boo
